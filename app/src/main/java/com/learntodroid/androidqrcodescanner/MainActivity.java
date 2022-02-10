403cb7394dcfda99efe6018d669a39f30e0916b9
carmenroblesc <98943816+carmenroblesc@users.noreply.github.com>
2022-02-10 09:56:52 +0100

    Add files via upload

--- app/src/main/java/com/learntodroid/androidqrcodescanner/MainActivity.java
+++ app/src/main/java/com/learntodroid/androidqrcodescanner/MainActivity.java
@@ -28,9 +28,11 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import org.json.JSONArray;
@@ -63,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
         Log.i("myTag", "onCreate funciona");
 
         previewView = findViewById(R.id.activity_main_previewView);
+        qrCode = "61505e4e7e438da72105168d";
+        obtenerDatosVolley(qrCode);
+
 
         qrCodeFoundButton = findViewById(R.id.activity_main_qrCodeFoundButton);
         qrCodeFoundButton.setVisibility(View.INVISIBLE);
@@ -70,13 +75,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), qrCode, Toast.LENGTH_SHORT).show();
+
                 Log.i(MainActivity.class.getSimpleName(), "QR Code Found: " + qrCode);
                 obtenerDatosVolley(qrCode);
-                /*try {
-                     String myJsonString = objeto.getString("ID");
-                } catch (JSONException e) {
-                 e.printStackTrace();
-                }*/
+           //     try {
+           //          String myJsonString = objeto.getString("ID");
+           //     } catch (JSONException e) {
+           //      e.printStackTrace();
+           //     }
 
                 Intent intent =new Intent(MainActivity.this, Imprimirdatos.class);
                 //intent.putExtra("json", arrayobjeto.toString());
@@ -88,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         requestCamera();
+
+
     }
 
     private void requestCamera() {
@@ -130,16 +138,58 @@ public class MainActivity extends AppCompatActivity {
         JSONObject dato= new JSONObject();
         Log.i("myTag", "función funciona");
 
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);  //@Carmen: te faltaba esto
+
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Log.i("myTag", "response funciona");
-               JSONObject dato = new JSONObject();
-
-                JSONArray jsonarray = new JSONArray();
-
                 try {
-                    dato =response.getJSONObject("producto");
+                    JSONObject productJSON =response.getJSONObject("product");
+                    JSONArray products = productJSON.getJSONArray("products");
+
+                    for(int i=0;i<products.length();i++) {
+                        String product_id= products.getString(i);
+                        Log.i("myTag", "producto "+i+":"+product_id);
+                    }
+
+                    String name = productJSON.getString("name");
+                    Log.i("myTag", "Nombre: "+name);
+
+                    String company = productJSON.getString("company");
+                    Log.i("myTag", "Empresa: "+company);
+
+                    String nutri_info = productJSON.getString("nutritionalInformation");
+                    Log.i("myTag", "Inform. nutricional: "+nutri_info);
+
+                    double pollution = productJSON.getDouble("pollutionGenerated");
+                    Log.i("myTag", "Contaminación generada: "+pollution);
+
+                    JSONObject shipmentJSON =response.getJSONObject("shipment_status");
+                    JSONArray status_array = shipmentJSON.getJSONArray("status");
+
+                    for(int i=0;i<status_array.length();i++) {
+                        JSONObject itemJSON= status_array.getJSONObject(i);
+                        String location_item = itemJSON.getJSONArray("currentLocation").toString();
+                        Log.i("myTag", "localización "+i+", coordenadas: "+location_item);
+
+                        String status_item = itemJSON.getString ("status");
+                        Log.i("myTag", "localización "+i+", estado: "+status_item);
+
+                        double distance_item = itemJSON.getDouble ("distance");
+                        Log.i("myTag", "localización "+i+", distancia: "+distance_item);
+
+                        double pollution_item = itemJSON.getDouble("pollutionGenerated");
+                        Log.i("myTag", "localización "+i+", contaminación generada: "+pollution_item);
+
+                        String registry_date_item = itemJSON.getString ("updatedAt");
+                        Log.i("myTag", "localización "+i+", fecha registro: "+registry_date_item);
+
+                    }
+
+
+
+
                     //jsonarray=response.getJSONArray(_qrCode);
                     asignarobjeto(dato);
                     //asignarArray(jsonarray);
@@ -157,26 +207,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Access the RequestQueue through your singleton class.
+        requestQueue.add(request); //@Carmen: te faltaba esto
+
 
     }
 
 
-   /* private void obtenerDatosVolley(String _qrCode) {
-        String url = "https://marketplace.gisai.geoide.upm.es/identifier/identifierJSON?id=" +_qrCode;
-        JSONObject dato= new JSONObject();
 
-        val jsonObjectRequest = JsonObjectRequest( Request.Method.GET,
-                url,
-                null,
-                Response.Listener { response ->
-                      textView.text = "Response: %s".format(response.toString())
-
-                },
-                 Response.ErrorListener { error ->
-                     // TODO: Handle error
-                }
-        )
-   }*/
 
 
 
